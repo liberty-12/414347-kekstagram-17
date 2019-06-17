1556edffe8b1a70df07e6cabdcc84fbcca0cadb3
Екатерина <liberty_12@mail.ru>
2019-06-17 16:44:08 +0300

    create functions for generating random and createObject function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var PHOTOS_NUMBER = 25;
+
 var comments = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -7,4 +9,33 @@ var comments = ['Всё отлично!',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var photoUrls = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+var getRandomElement = function (arr) {
+  return arr[Math.floor((Math.random() * arr.length))];
+};
+
+var getRandomInteger = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+var createObject = function (commentArray) {
+  var photoDescriptions = [];
+
+  for (var i = 0; i < PHOTOS_NUMBER; i++) {
+    var x = getRandomInteger(1, 25);
+    var y = getRandomInteger(15, 200);
+
+    var photoDescription = {
+      'avatar': 'photos/' + x + '.jpg',
+      'comment': getRandomElement(commentArray),
+      'likes': y
+    };
+  }
+
+  photoDescriptions.push(photoDescription);
+
+  return photoDescriptions;
+};
+
+var descriptions = createObject(comments);
+
+console.log(descriptions);
