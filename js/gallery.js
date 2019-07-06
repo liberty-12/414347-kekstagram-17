a9aa8ca0e1f53be91faf220de6c4108ecc09d6fc
Екатерина <liberty_12@mail.ru>
2019-07-06 12:48:41 +0300

    create gallery module

---
+++ js/gallery.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Rendering pictures
+
+(function () {
+  var pictures = document.querySelector('.pictures');
+  var pictureTemplate = document
+    .querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+  var renderPicture = function (photo) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+
+    return pictureElement;
+  };
+
+  var renderPhotosFragment = function (array) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < array.length; i++) {
+      fragment.appendChild(renderPicture(array[i]));
+    }
+
+    pictures.appendChild(fragment);
+  };
+
+  renderPhotosFragment(window.data.photos);
+})();
