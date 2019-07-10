bee0d44124020b558490c5a374cd2f34cbefca5f
Екатерина <liberty_12@mail.ru>
2019-07-10 14:14:51 +0300

    move rendering pictures from data to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,58 @@
 'use strict';
 
-// Rendering pictures
+// RENDERING PICTURES
 
 (function () {
-  var pictures = document.querySelector('.pictures');
-  var pictureTemplate = document
-    .querySelector('#picture')
-    .content
-    .querySelector('.picture');
+  var successHandler = function (data) {
+    var photos = data;
+    var pictures = document.querySelector('.pictures');
+    var pictureTemplate = document
+      .querySelector('#picture')
+      .content
+      .querySelector('.picture');
 
-  var renderPicture = function (photo) {
-    var pictureElement = pictureTemplate.cloneNode(true);
+    var renderPicture = function (photo) {
+      var pictureElement = pictureTemplate.cloneNode(true);
 
-    pictureElement.querySelector('.picture__img').src = photo.url;
-    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
-    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+      pictureElement.querySelector('.picture__img').src = photo.url;
+      pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+      pictureElement.querySelector('.picture__likes').textContent = photo.likes;
 
-    return pictureElement;
+      return pictureElement;
+    };
+
+    var renderPhotosFragment = function (array) {
+      var fragment = document.createDocumentFragment();
+
+      for (var i = 0; i < array.length; i++) {
+        fragment.appendChild(renderPicture(array[i]));
+      }
+
+      pictures.appendChild(fragment);
+    };
+
+    renderPhotosFragment(photos);
   };
 
-  var renderPhotosFragment = function (array) {
-    var fragment = document.createDocumentFragment();
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
 
-    for (var i = 0; i < array.length; i++) {
-      fragment.appendChild(renderPicture(array[i]));
-    }
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.zIndex = '100';
+    node.style.width = '500px';
+    node.style.margin = '0 auto';
+    node.style.padding = '20px';
+    node.style.backgroundColor = 'red';
+    node.style.fontSize = '18px';
+    node.style.lineHeight = '22px';
+    node.style.textAlign = 'center';
+    node.style.color = '#ffffff';
 
-    pictures.appendChild(fragment);
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
   };
 
-  renderPhotosFragment(window.data.photos);
+  window.load.load(successHandler, errorHandler);
 })();
