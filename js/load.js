383213131aac662354c835b88a96e15e97491fdd
Екатерина <liberty_12@mail.ru>
2019-07-10 14:12:56 +0300

    add extra spaces

--- js/load.js
+++ js/load.js
@@ -15,9 +15,11 @@
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
       });
+
       xhr.addEventListener('error', function () {
         onError('Произошла ошибка соединения');
       });
+
       xhr.addEventListener('timeout', function () {
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
       });
