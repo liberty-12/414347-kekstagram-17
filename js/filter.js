014ad0aa3be04e84935b53a69bbef5306b92fe80
Екатерина <liberty_12@mail.ru>
2019-07-11 15:41:08 +0300

    set timeout for deleting class

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,8 @@
 'use strict';
 
 (function () {
-
+  var imgFilters = document.querySelector('.img-filters');
+  setTimeout(function () {
+    imgFilters.classList.remove('img-filters--inactive');
+  }, 500);
 })();
