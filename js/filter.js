75700624a16c857714e0ce3910631a45cda0a5f0
Екатерина <liberty_12@mail.ru>
2019-07-11 17:26:25 +0300

    change active button

--- js/filter.js
+++ js/filter.js
@@ -5,4 +5,23 @@
   setTimeout(function () {
     imgFilters.classList.remove('img-filters--inactive');
   }, 500);
+
+  var imgFiltersForm = document.querySelector('.img-filters__form');
+  var activeFilterButton = document.querySelector('.img-filters__button--active');
+
+  // var Filter = {
+  //   'popular': 'popular',
+  //   'new': 'new',
+  //   'discussed': 'discussed'
+  // };
+
+  imgFiltersForm.addEventListener('click', function (evt) {
+    var target = evt.target;
+    if ((target.tagName === 'BUTTON') && (!target.classList.contains('img-filters__button--active'))) {
+      activeFilterButton.classList.remove('img-filters__button--active');
+      target.classList.add('img-filters__button--active');
+
+      activeFilterButton = target;
+    }
+  });
 })();
