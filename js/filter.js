9add6b8c2fd7015c2fb3bcbcd61ba79eaa4a7289
Екатерина <liberty_12@mail.ru>
2019-07-12 18:33:05 +0300

    comment

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,70 @@
 'use strict';
 
 (function () {
+  // var galleryScript = document.querySelector('.gallery-script');
+  //
+  // galleryScript.onload = function () {
+  //   console.log("success!!")
+  //   var imgFilters = document.querySelector('.img-filters');
+  //   setTimeout(function () {
+  //     imgFilters.classList.remove('img-filters--inactive');
+  //   }, 500);
+  //
+  //   var imgFiltersForm = document.querySelector('.img-filters__form');
+  //   var activeFilterButton = document.querySelector('.img-filters__button--active');
+  //
+  //   var sort = function (array, sortName) {
+  //     switch (sortName) {
+  //       case 'new':
+  //         array = array.sort(function () {
+  //           return window.util.getRandomInteger(0, 100) - window.util.getRandomInteger(0, 100);
+  //         }).slice(0, 10);
+  //         break;
+  //
+  //       case 'discussed':
+  //         array.sort(function (left, right) {
+  //           return right.comments.length - left.comments.length;
+  //         });
+  //         break;
+  //     }
+  //
+  //     return array;
+  //   };
+  //
+  //   var lastTimeout;
+  //   var debounce = function (action, last) {
+  //     window.clearTimeout(last);
+  //     last = window.setTimeout(action, 500);
+  //   };
+  //
+  //   imgFiltersForm.addEventListener('click', function (evt) {
+  //     var target = evt.target;
+  //     if ((target.tagName === 'BUTTON') && (!target.classList.contains('img-filters__button--active'))) {
+  //       activeFilterButton.classList.remove('img-filters__button--active');
+  //       target.classList.add('img-filters__button--active');
+  //
+  //       var filterId = target.id;
+  //       var sortedPhotos;
+  //
+  //       switch (filterId) {
+  //         case 'filter-new':
+  //           sortedPhotos = sort(window.gallery.photos.slice(), 'new');
+  //           debounce(window.gallery.updatePhotos(sortedPhotos), lastTimeout);
+  //           break;
+  //         case 'filter-discussed':
+  //           sortedPhotos = sort(window.gallery.photos.slice(), 'discussed');
+  //           debounce(window.gallery.updatePhotos(sortedPhotos), lastTimeout);
+  //           break;
+  //         default:
+  //           debounce(window.gallery.updatePhotos(window.gallery.photos.slice()), lastTimeout);
+  //           break;
+  //       }
+  //
+  //       activeFilterButton = target;
+  //     }
+  //   });
+  // };
+
   var imgFilters = document.querySelector('.img-filters');
   setTimeout(function () {
     imgFilters.classList.remove('img-filters--inactive');
