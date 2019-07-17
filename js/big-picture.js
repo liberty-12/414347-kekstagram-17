1e7e6dc0bed7f953abca1e5601575ed8783e6336
Екатерина <liberty_12@mail.ru>
2019-07-17 20:50:58 +0300

    test

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,5 +1,18 @@
 'use strict';
 
 (function () {
+  var bigPicture = document.querySelector('.big-picture');
+  var commentCount = bigPicture.querySelector('.social__comment-count');
+  var commentLoader = bigPicture.querySelector('.comments-loader');
+  commentCount.classList.add('visually-hidden');
+  commentLoader.classList.add('visually-hidden');
 
+  var pictures = document.querySelector('.pictures');
+
+  pictures.addEventListener('click', function (evt) {
+    var target = evt.target;
+    if (target.tagName === 'IMG') {
+      target.parentNode.querySelector('.big-picture').classList.remove('hidden');
+    }
+  });
 })();
