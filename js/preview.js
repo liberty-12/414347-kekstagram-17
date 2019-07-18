22d373c52e69eb0574b18c37baf3d1de7275c982
Екатерина <liberty_12@mail.ru>
2019-07-18 16:17:26 +0300

    describe error and succsess messages behavior

--- js/preview.js
+++ js/preview.js
@@ -35,4 +35,64 @@
   uploadClose.addEventListener('click', function () {
     closeUploadPopup();
   });
+
+  var submitButton = document.querySelector('.img-upload__submit');
+  var mainField = document.querySelector('main');
+
+  var onMessageEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeMessagePopup();
+    }
+  };
+
+  var onMessageClick = function () {
+    closeMessagePopup();
+  };
+
+  var closeMessagePopup = function () {
+    document.querySelector('.error').remove(); // как передать в функцию имя класса?
+    document.removeEventListener('keydown', onMessageEscPress);
+    document.removeEventListener('click', onMessageClick);
+  };
+
+  // SUCCESS
+  var successMessageTemplate = document
+    .querySelector('#success')
+    .content
+    .querySelector('.success');
+  var successMessage = successMessageTemplate.cloneNode(true);
+  var successCloseButoon = successMessage.querySelector('.success__button');
+
+  var successHandler = function () {
+    closeUploadPopup();
+    document.addEventListener('keydown', onMessageEscPress);
+    document.addEventListener('click', onMessageClick);
+    successCloseButoon.addEventListener('click', closeMessagePopup);
+    mainField.appendChild(successMessage);
+  };
+
+  // ERROR
+  var errorMessageTemplate = document
+    .querySelector('#error')
+    .content
+    .querySelector('.error');
+
+  var errorMessage = errorMessageTemplate.cloneNode(true);
+  var errorCloseButoon = errorMessage.querySelector('.error__buttons');
+
+  var errorHandler = function () {
+    closeUploadPopup();
+    document.addEventListener('keydown', onMessageEscPress);
+    document.addEventListener('click', onMessageClick);
+    errorCloseButoon.addEventListener('click', closeMessagePopup);
+    mainField.appendChild(errorMessage);
+  };
+
+
+  // EVENT
+  submitButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    window.send.send(successHandler, errorHandler);
+  });
 })();
