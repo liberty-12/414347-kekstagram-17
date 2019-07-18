d4b6b20bc51bdabac209da31d49640b509e6a325
Екатерина <liberty_12@mail.ru>
2019-07-18 19:55:07 +0300

    fix send

--- js/send.js
+++ js/send.js
@@ -4,29 +4,24 @@
   var URL = 'https://js.dump.academy/kekstagram';
 
   window.send = {
-    send: function (onSuccess, onError) {
+    send: function (data, onSuccess, onError) {
       var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
         if (xhr.status === 200) {
           onSuccess(xhr.response);
         } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+          onError();
         }
       });
 
       xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
+        onError();
       });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000; // 10s
 
       xhr.open('POST', URL);
-      xhr.send();
+      xhr.send(data);
     }
   };
 })();
