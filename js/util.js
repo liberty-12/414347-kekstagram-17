fca0d1b4678a8910c4a1b5d71165de379273fa22
Екатерина <liberty_12@mail.ru>
2019-07-23 15:46:31 +0300

    delete unused functions

--- js/util.js
+++ js/util.js
@@ -1,11 +1,6 @@
 'use strict';
 
 (function () {
-  var getRandomElementFromArray = function (arr) {
-    return arr[Math.floor((Math.random() * arr.length))];
-  };
-
-  // 27
   var ESC_CODE = 27;
 
   window.util = {
@@ -19,30 +14,13 @@
       return Math.floor(min + Math.random() * (max + 1 - min));
     },
 
-    getNewLengthArray: function (arr) {
-      var newArray = [];
-      var newArrayLength = window.util.getRandomInteger(1, arr.length);
-
-      for (var i = 0; i < newArrayLength; i++) {
-        newArray.push(getRandomElementFromArray(arr));
+    checkSameElements: function (array, elem) {
+      var isSame = false;
+      if (array.indexOf(elem) > -1) {
+        isSame = true;
       }
 
-      return newArray;
-    },
-
-    getMixedArray: function (elementsLength) {
-      var elementsArray = [];
-      var element = window.util.getRandomInteger(1, elementsLength);
-
-      while (elementsArray.length < elementsLength) {
-        if (elementsArray.indexOf(element) > -1) {
-          element = window.util.getRandomInteger(1, elementsLength);
-        } else {
-          elementsArray.push(element);
-        }
-      }
-
-      return elementsArray;
+      return isSame;
     }
   };
 })();
