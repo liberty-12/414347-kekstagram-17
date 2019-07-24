205e93272363019b80370728d1c0d0606f089cad
Екатерина <liberty_12@mail.ru>
2019-07-24 14:38:01 +0300

    fix checkSameElement function

--- js/util.js
+++ js/util.js
@@ -14,13 +14,20 @@
       return Math.floor(min + Math.random() * (max + 1 - min));
     },
 
-    checkSameElements: function (array, elem) {
-      var isSame = false;
-      if (array.indexOf(elem) > -1) {
-        isSame = true;
+    checkSameElements: function (array) {
+      var newArray = [];
+      var hasSameElement = false;
+
+      for (var i = 0; i < array.length; i++) {
+        if (newArray.indexOf(array[i].toLowerCase()) === -1) {
+          newArray.push(array[i]);
+        } else {
+          hasSameElement = true;
+          break;
+        }
       }
 
-      return isSame;
+      return hasSameElement;
     }
   };
 })();
